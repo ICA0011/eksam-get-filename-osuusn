c42de5e32e04c84a83d6aefd03ef332cea681b0e
osuusn <78435470+osuusn@users.noreply.github.com>
2022-05-30 13:14:40 +0300

    Init

--- get_filename.py
+++ get_filename.py
@@ -3,8 +3,10 @@ from bs4 import BeautifulSoup
 
 url = "http://upload.itcollege.ee/~aleksei/random_files/"
 
-def get_filename(url):
-  
-  # your code here
-  
-  return filename
+def get_filename():
+  file_names = []
+  soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+  for i in soup.find_all('a'):
+    file_name = f"{url}{i.get('href')}"
+    file_names.append(file_name)
+  return file_names[10]
